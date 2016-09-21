def8743ae795da42bc6509b1abe53e8aa45d417f
Norayr Chilingarian <norayr@arnet.am>
2016-09-21 15:07:55 +0400

    changes.

--- mount-sd.custom.sh
+++ mount-sd.custom.sh
@@ -7,9 +7,11 @@ then
 if [ "$ACTION" = "add" ]; then
   mount /dev/mmcblk1p1 /mnt/sd
   mount -o bind /mnt/sd/.home /home/nemo
-  mount -o bind /mnt/sd/.android /data/sdcard
+  mount -o bind /mnt/sd/.android /data/media
+  mount -o bind /mnt/sd/.data /data/data
 else
-  umount /data/sdcard
+  umount /data/media
+  umount /data/data
   umount /home/nemo
   umount /mnt/sd
 fi
