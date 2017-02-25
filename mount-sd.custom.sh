f26686ae060dd1020fce5f643834c83d13a04f1e
norayr <norayr@arnet.am>
2017-02-25 20:14:22 +0400

    ext4 optimizations; -- noch

--- mount-sd.custom.sh
+++ mount-sd.custom.sh
@@ -5,7 +5,7 @@ device0="/dev/mmcblk1p1"
 if [ -b "$device0" ]
 then
 if [ "$ACTION" = "add" ]; then
-  mount /dev/mmcblk1p1 /mnt/sd
+  mount -o noatime,data=writeback,barrier=0,nobh /dev/mmcblk1p1 /mnt/sd
   mount -o bind /mnt/sd/.home /home/nemo
   mount -o bind /mnt/sd/.android /data/media
   mount -o bind /mnt/sd/.data /data/data
